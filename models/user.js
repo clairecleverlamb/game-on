8cb57c5cd69708c11764e0cb30ee84f48bad3bdb
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-16 16:59:31 -0700

    wip: added folder structures and user models

--- models/user.js
+++ models/user.js
@@ -4,11 +4,53 @@ const userSchema = mongoose.Schema({
   username: {
     type: String,
     required: true,
+    unique: true,
   },
   password: {
     type: String,
     required: true,
   },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  fullName: {
+    type: String,
+  }, 
+  profilePicture: {
+    type: String,
+  }, 
+  location: {
+    type: String,
+  }, 
+  sportsInterests: {
+    type: String,
+    enum: ['Beginner', 'Intermediate', 'Advance'],
+  }, 
+  stats: {
+    gamePlayed: {
+      type: Number, 
+      default: 0,
+    },
+    wins: {
+      type: Number,
+      default: 0, 
+    },
+  },
+  pastGames: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Game',
+  }],
+  tournamentJoined: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Tournament'
+  }],
+  highlightVideos: [{
+    type: String,
+  }],
+}, {
+  timestamps: true, 
 });
 
 const User = mongoose.model('User', userSchema);
