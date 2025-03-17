fb908d2cf0fd4db6a0ff37fa7ee7c921662dc7dd
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-17 16:07:22 -0700

    wip: added config files

---
+++ config/passport-config.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/user.js');
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: '/auth/google/callback'
+  },
+  async (accessToken, refreshToken, profile, done) => {
+    try {
+      let user = await User.findOne({ googleId: profile.id });
+      if (!user) {
+        user = new User({
+          googleId: profile.id,
+          username: profile.displayName, 
+          email: profile.emails[0].value 
+        });
+        await user.save();
+      }
+      done(null, user); // Pass user to Passport
+    } catch (error) {
+      done(error, null);
+    }
+  }
+));
+
+// Serialize user into session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize user from session
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
+
+module.exports = passport;
