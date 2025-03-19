686213ba4dde848f72b6ec8971a7473875a58b79
clairecleverlamb <clairechenszeying@gmail.com>
2025-03-19 14:26:32 -0700

    wip: added features for pass-view middleware

--- middleware/pass-user-to-view.js
+++ middleware/pass-user-to-view.js
@@ -1,8 +1,20 @@
-const passUserToView = (req, res, next) => {
-    res.locals.user = req.session.user ? req.session.user : null;
-    next();
+const User = require('../models/user.js');
+
+const passUserToView = async (req, res, next) => {
+  if (req.session.user && req.session.user._id) {
+    try {
+      const user = await User.findById(req.session.user._id)
+        .select('username fullName profilePicture stats pastGames tournamentJoined') 
+        .populate('tournamentJoined'); 
+      res.locals.user = user || null; 
+    } catch (error) {
+      console.log('Error fetching user in middleware:', error);
+      res.locals.user = null; 
+    }
+  } else {
+    res.locals.user = null; 
+  }
+  next();
 };
-  
 
 module.exports = passUserToView;
-  
